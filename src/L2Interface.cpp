a964ea3a905d18a311e74ad1872a8ea949673e2c
Ravi C <clevert.pretto@gmail.com>
2021-07-11 21:57:21 +0530

    updated L2Interface

--- src/L2Interface.cpp
+++ src/L2Interface.cpp
@@ -5,181 +5,186 @@
 
 using namespace std;
 
-#define PIN_CE 17 // (chip enable)
-#define PIN_CSN 0 // (chip select not)
+#define PIN_CE 	17 // (chip enable)
+#define PIN_CSN 0  // (chip select not)
 
 uint8_t pipeNumber;
 uint8_t payloadSize;
-uint64_t TXaddress = 0x7878787878LL;
 
-RF24_Init_S sRFInitData =
+L2Interface::L2Interface(RF24 *radioObj)
 {
-	.channel = 5,
-	.PALevel = RF24_PA_HIGH,
-	.dataRate = RF24_1MBPS,
-	.child = 0,
-	.address = 0x7878787878LL,
-	.enableAutoACK = true,
-	.TXretryDelay = 15, /*15 x 250us = 4000 us*/
-	.TXretryCount = 15 /*retry for 15 times*/
-};
+	L2radioObj = radioObj;
 
-void L2_Initialise(RF24 *radioObj, RF24_Init_S *sRFInitData)
+	sRFInitData.channel = 5;// 5 - at a frequency of 2.405 GHz
+	sRFInitData.PALevel = RF24_PA_HIGH;
+	sRFInitData.dataRate = RF24_1MBPS;
+	sRFInitData.child = 0;
+	sRFInitData.RXaddress = 0x7878787878LL;
+	sRFInitData.enableAutoACK = true;
+	sRFInitData.TXretryDelay = 15; /*15 x 250us = 4000 us*/
+	sRFInitData.TXretryCount = 15; /*retry for 15 times*/
+	sRFInitData.TXaddress = 0x7878787878LL;
+}
+
+void L2Interface::L2_Initialise(void)
 {
-	radioObj->begin();
-	// data transmission channel (from 0 to 125), 5 - at a frequency of 2.405 GHz
-	radioObj->setChannel(sRFInitData->channel);
-	// signal gain level (RF24_PA_MIN = -18dBm, RF24_PA_LOW = -12dBm, RF24_PA_HIGH = -6dBm, RF24_PA_MAX = 0dBm)
-	radioObj->setPALevel(sRFInitData->PALevel);
-	// data transfer rate (RF24_250KBPS, RF24_1MBPS, RF24_2MBPS), RF24_1MBPS - 1Mbps
-	radioObj->setDataRate(sRFInitData->dataRate);
-	// open the pipe with the address 0x7878787878LL to receive data (there can be pipes 0 - 5 in total)
-	radioObj->openReadingPipe(sRFInitData->child, sRFInitData->address);
+	L2radioObj->begin();
+	L2radioObj->setChannel(sRFInitData.channel);
+	L2radioObj->setPALevel(sRFInitData.PALevel);
+	L2radioObj->setDataRate(sRFInitData.dataRate);
+	L2radioObj->openReadingPipe(sRFInitData.child, sRFInitData.RXaddress);
 
 	// we allow to place user data in the acknowledgment packet
-	radioObj->enableAckPayload();
+	L2radioObj->enableAckPayload();
 
 	// we allow sending to the transmitter a packet of acknowledgment of its data reception on all pipes (by default, true)
-	radioObj->setAutoAck(sRFInitData->enableAutoACK);
+	L2radioObj->setAutoAck(sRFInitData.enableAutoACK);
 
 	// allow dynamically re-sizable payload size on all pipes (max 32 bytes)
-	radioObj->enableDynamicPayloads();
+	L2radioObj->enableDynamicPayloads();
 }
 
-void L2_printRFDetails(RF24 *radioObj)
+void L2Interface::L2_printRFDetails(void)
 {
-	radioObj->printDetails();
+	L2radioObj->printDetails();
 }
 
-void L2_ReceiveMode(RF24 *radioObj)
+void L2Interface::L2_ReceiveMode(void)
 {
-	radioObj->startListening();
+	L2radioObj->startListening();
 }
 
-bool L2_DataAvailable(RF24 *radioObj, uint8_t* pipe_num)
+bool L2Interface::L2_DataAvailable(uint8_t* pipe_num)
 {
-	return(radioObj->available(pipe_num));
+	return(L2radioObj->available(pipe_num));
 }
 
-uint8_t L2_payLoadSize(RF24 *radioObj)
+uint8_t L2Interface::L2_payLoadSize(void)
 {
-	return(radioObj->getDynamicPayloadSize());
+	return(L2radioObj->getDynamicPayloadSize());
 }
 
-void L2_readData(RF24 *radioObj, void* buf, uint8_t len)
+void L2Interface::L2_readData(void* buf, uint8_t len)
 {
-	radioObj->read(buf, len);
+	L2radioObj->read(buf, len);
 }
 
-void L2_writeACKpayLoad(RF24 *radioObj, uint8_t pipe, const void* buf, uint8_t len)
+void L2Interface::L2_writeACKpayLoad(uint8_t pipe, const void* buf, uint8_t len)
 {
-	radioObj->writeAckPayload(pipe, buf, len);
+	L2radioObj->writeAckPayload(pipe, buf, len);
 }
 
-void L2_setRetries(RF24 *radioObj, uint8_t delay, uint8_t count)
+void L2Interface::L2_setRetries(void)
 {
-	radioObj->setRetries(delay, count);
+	L2radioObj->setRetries(sRFInitData.TXretryDelay, sRFInitData.TXretryCount);
 }
 
-void L2_openWritingPipe(RF24 *radioObj, uint64_t address)
+void L2Interface::L2_openWritingPipe(void)
 {
-	radioObj->openWritingPipe(address);
+	L2radioObj->openWritingPipe(sRFInitData.TXaddress);
 }
 
-bool L2_writeData(RF24 *radioObj, const void* buf, uint8_t len)
+bool L2Interface::L2_writeData(const void* buf, uint8_t len)
 {
-	return(radioObj->write(buf, len));
+	return(L2radioObj->write(buf, len));
 }
 
-bool L2_isACKPayloadAvailable(RF24 *radioObj)
+bool L2Interface::L2_isACKPayloadAvailable(void)
 {
-	return(radioObj->isAckPayloadAvailable());
+	return(L2radioObj->isAckPayloadAvailable());
 }
 
-bool L2_isRXFifoFull(RF24 *radioObj)
+bool L2Interface::L2_isRXFifoFull(void)
 {
-	return(radioObj->rxFifoFull());
+	return(L2radioObj->rxFifoFull());
 }
 
-uint8_t L2_flushRXBuffer(RF24 *radioObj)
+uint8_t L2Interface::L2_flushRXBuffer(void)
 {
-	return(radioObj->flush_rx());
+	return(L2radioObj->flush_rx());
 }
 
-//int main() {
-//	RF24 radio(PIN_CE, PIN_CSN); // create a radio object
-//
-//	L2_Initialise(&radio, &sRFInitData);
-//
-//	// module configuration output
-//	L2_printRFDetails(&radio);
-//
-//	// turn on the receiver, start listening to open pipes
-//	L2_ReceiveMode(&radio);
-//
-//	cout << "Start listening..." << endl;
-//
-//  while (true) {
-//
-//    // If there is received data in the buffer, then we transfer the address of the variable (must be of the uint8_t type)
-//	// where you want to put the pipe number on which the data was received
-//    if (L2_DataAvailable(&radio, &pipeNumber)) {
-//
-//      payloadSize = L2_payLoadSize(&radio); // get the size of the received payload
-//
-//      char payload[payloadSize];
-//      string receivedData;
-//
-//      // We read the received data into the payload array specifying the size of this array in bytes
-//      L2_readData(&radio, &payload, payloadSize);
-//
-//      for (uint8_t i = 0; i < payloadSize; i++) {
-//        receivedData += payload[i];
-//      }
-//
-//      cout << "Pipe number : " << (int) pipeNumber << " ";
-//      cout << "Payload size : " << (int) payloadSize << " ";
-//      cout << "Data: " << receivedData << endl;
-//
-//      char ackData[] = "Data from buffer";
-//
-//      // We put the data into the FIFO buffer. As soon as the packet is received, the data from the buffer
-//      // will be sent to this transmitter along with an acknowledgment packet of its data
-//      L2_writeACKpayLoad(&radio, pipeNumber, &ackData, sizeof(ackData));
-//    }
-//  }
-//  return 0;
-//}
+int RX_Task() {
+	RF24 radio(PIN_CE, PIN_CSN); // create a radio object
 
-int main() {
+	L2Interface L2Comm(&radio);
+
+	L2Comm.L2_Initialise();
+
+	// module configuration output
+	L2Comm.L2_printRFDetails();
+
+	// turn on the receiver, start listening to open pipes
+	L2Comm.L2_ReceiveMode();
+
+	cout << "Start listening..." << endl;
+
+  while (true) {
+
+    // If there is received data in the buffer, then we transfer the address of the variable (must be of the uint8_t type)
+	// where you want to put the pipe number on which the data was received
+    if (L2Comm.L2_DataAvailable(&pipeNumber)) {
+
+      payloadSize = L2Comm.L2_payLoadSize(); // get the size of the received payload
+
+      char payload[payloadSize];
+      string receivedData;
+
+      // We read the received data into the payload array specifying the size of this array in bytes
+      L2Comm.L2_readData(&payload, payloadSize);
+
+      for (uint8_t i = 0; i < payloadSize; i++) {
+        receivedData += payload[i];
+      }
+
+      cout << "Pipe number : " << (int) pipeNumber << " ";
+      cout << "Payload size : " << (int) payloadSize << " ";
+      cout << "Data: " << receivedData << endl;
+
+      char ackData[] = "Data from buffer";
+
+      // We put the data into the FIFO buffer. As soon as the packet is received, the data from the buffer
+      // will be sent to this transmitter along with an acknowledgment packet of its data
+      L2Comm.L2_writeACKpayLoad(pipeNumber, &ackData, sizeof(ackData));
+    }
+  }
+  return 0;
+}
+
+int TX_Task() {
 
   RF24 radio(PIN_CE, PIN_CSN); // create a radio object
 
-  	L2_Initialise(&radio, &sRFInitData);
+  L2Interface L2Comm(&radio);
+
+  L2Comm.L2_Initialise();
 
   	// module configuration output
-  	L2_printRFDetails(&radio);
+  L2Comm.L2_printRFDetails();
+
   // If the transmitter has not received a response from the receiver, then it will retry sending data.
   // Wait between each retry, in multiples of 250,
   // the maximum is 15.0 means 250 μs, (15 * 250) +250 = 4000 μs,
   // and the number of attempts before surrender, maximum 15
   // the method is only available for the transmitter
-  L2_setRetries(&radio, sRFInitData.TXretryDelay, sRFInitData.TXretryCount);
+  L2Comm.L2_setRetries();
 
   // open a pipe with a unique ID (only one pipe can be opened at a time for data transfer)
-  L2_openWritingPipe(&radio, TXaddress);
+  L2Comm.L2_openWritingPipe();
+
   // payload block can be up to 32 bytes
   char text[] = "Hello world!";
   char ackData[24];
 
-  while (true) {
+  while (true)
+  {
 
     // By default, the transmitter asks the receiver for confirmation of receipt of data,
     // and the receiver, having received the data and checking the CRC, returns an acknowledgment packet to the sender.
 
     // Pass the address of the variable and its size in bytes, the value of which will be used as payload
     // And check for a returned data receipt packet
-    if (L2_writeData(&radio, &text, sizeof(text)))
+    if (L2Comm.L2_writeData(&text, sizeof(text)))
     {
       cout << "Delivered " << (int) sizeof(text) << " byte" << endl;
     }
@@ -189,25 +194,33 @@ int main() {
     }
 
     // If the buffer contains received data from the acknowledgment packet
-    if (L2_isACKPayloadAvailable(&radio))
+    if (L2Comm.L2_isACKPayloadAvailable())
     {
 		// read data from the buffer into the ackData array indicating how many bytes can fit into the array
-		L2_readData(&radio, &ackData, sizeof(ackData));
+    	L2Comm.L2_readData(&ackData, sizeof(ackData));
 		string FIFO;
 		for (uint8_t i = 0; i < sizeof(ackData); i++)
 		{
 			FIFO += ackData[i];
 		}
-		//Serial.println(FIFO);
 
 		// If all three FIFO buffers are full then clear
-		if (L2_isRXFifoFull(&radio)) {
+		if (L2Comm.L2_isRXFifoFull()) {
 			cout << "Buffer full" << endl;
-			L2_flushRXBuffer(&radio); // clear buffer
+			L2Comm.L2_flushRXBuffer(); // clear buffer
 		}
     }
 
     delay(1000);
   }
 
+  return 0;
+}
+
+int main()
+{
+	TX_Task();
+	RX_Task();
+
+	return 0;
 }
--- src/L2Interface.h
+++ src/L2Interface.h
@@ -3,38 +3,65 @@
 
 typedef struct
 {
-	uint8_t channel;
-	rf24_pa_dbm_e PALevel;
-	rf24_datarate_e dataRate;
-	uint8_t child;
-	uint64_t address;
+	uint8_t channel; // data transmission channel (from 0 to 125)
+	rf24_pa_dbm_e PALevel; //signal gain level (RF24_PA_MIN = -18dBm, RF24_PA_LOW = -12dBm, RF24_PA_HIGH = -6dBm, RF24_PA_MAX = 0dBm)
+	rf24_datarate_e dataRate; // data transfer rate (RF24_250KBPS, RF24_1MBPS, RF24_2MBPS), RF24_1MBPS - 1Mbps
+	uint8_t child; // there can be pipes 0 - 5 in total
+	uint64_t RXaddress; //The pipe with the address
 	bool enableAutoACK;
 	uint8_t TXretryDelay;
 	uint8_t TXretryCount;
+	uint64_t TXaddress; //The pipe with the address
 }RF24_Init_S;
 
-void L2_Initialise(RF24 *radioObj, RF24_Init_S *sRFInitData);
+class L2Interface
+{
+	//Attributes
+	public:
 
-void L2_printRFDetails(RF24 *radioObj);
+	private:
+		RF24 *L2radioObj;
+		RF24_Init_S sRFInitData;
+	protected:
 
-void L2_ReceiveMode(RF24 *radioObj);
 
-bool L2_DataAvailable(RF24 *radioObj, uint8_t* pipe_num);
+	//Methods
+	public:
 
-uint8_t L2_payLoadSize(RF24 *radioObj);
+		//Constructor
+		L2Interface(RF24 *radioObj);
 
-void L2_readData(RF24 *radioObj, void* buf, uint8_t len);
+		//Distructor
+		~L2Interface(void);
 
-void L2_writeACKpayLoad(RF24 *radioObj, uint8_t pipe, const void* buf, uint8_t len);
+		void L2_Initialise(void);
 
-void L2_setRetries(RF24 *radioObj, uint8_t delay, uint8_t count);
+		void L2_printRFDetails(void);
 
-void L2_openWritingPipe(RF24 *radioObj, uint64_t address);
+		void L2_ReceiveMode(void);
 
-bool L2_writeData(RF24 *radioObj, const void* buf, uint8_t len);
+		bool L2_DataAvailable(uint8_t* pipe_num);
 
-bool L2_isACKPayloadAvailable(RF24 *radioObj);
+		uint8_t L2_payLoadSize(void);
 
-bool L2_isRXFifoFull(RF24 *radioObj);
+		void L2_readData(void* buf, uint8_t len);
 
-uint8_t L2_flushRXBuffer(RF24 *radioObj);
+		void L2_writeACKpayLoad(uint8_t pipe, const void* buf, uint8_t len);
+
+		void L2_setRetries(void);
+
+		void L2_openWritingPipe(void);
+
+		bool L2_writeData(const void* buf, uint8_t len);
+
+		bool L2_isACKPayloadAvailable(void);
+
+		bool L2_isRXFifoFull(void);
+
+		uint8_t L2_flushRXBuffer(void);
+
+	private:
+
+	protected:
+
+};
